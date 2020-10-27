0cb7d6dddf89314edf8be5492f92dc6ae155ac70
Ehmad Saeed <justehmadsaeed@gmail.com>
2020-10-28 04:12:37 +0500

    📦 NEW: Implement Student class

---
+++ Lab 2/student.cs
@@ -0,0 +1,99 @@
+﻿using System;
+
+namespace cSharp {
+    public class Student {
+        private string name;
+        public string studentName {
+            get => name;
+            set {
+                bool valid = false;
+                do {
+                    for (int i = 0; i < value.Length; i++) {
+                        if (char.IsLetter(value[i]) || value[i] == ' ')
+                            valid = true;
+                        else {
+                            valid = false;
+                            break;
+                        }
+                    }
+                    if (!valid) {
+                        Console.WriteLine("Name must contain only letters.");
+                        value = Console.ReadLine();
+                    }
+                } while (!valid);
+                name = value;
+            }
+        }
+        private string reg;
+        public string reg_no {
+            get => reg;
+            set {
+                bool valid = false;
+                do {
+                    if (value.Length > 8) {
+
+                        for (int i = 0; i < value.Length; i++) {
+                            if (i < 4)
+                                if (!char.IsDigit(value[i])) {
+                                    valid = false;
+                                    break;
+                                }
+                            else
+                                valid = true;
+
+                            if (i > 4 && i <= 6)
+                                if (!char.IsLetter(value[i])) {
+                                    valid = false;
+                                    break;
+                                }
+                            else
+                                valid = true;
+                            if (i > 7)
+                                if (!char.IsDigit(value[i])) {
+                                    valid = false;
+                                    break;
+                                }
+                            else
+                                valid = true;
+                        }
+                        if (value[4] != '-' || value[7] != '-')
+                            valid = false;
+                    }
+
+                    if (!valid) {
+                        Console.WriteLine("Registration Number must be valid (2018-CS-123).");
+                        value = Console.ReadLine();
+                    }
+                } while (!valid);
+                reg = value;
+            }
+        }
+        private string deg;
+        public string degree {
+            get => deg;
+            set {
+                while (!(value == "MS" || value == "BS" || value == "BE")) {
+                    Console.WriteLine("Degree should be MS, BS or BE");
+                    value = Console.ReadLine();
+                }
+                deg = value;
+            }
+        }
+
+        // Default Constructor with no parameters
+        public Student() {
+            this.studentName = "Ehmad Saeed";
+            this.reg_no = "2018-CS-1";
+            this.degree = "BS";
+
+        }
+        // Destructor
+        ~Student() {
+            Console.WriteLine("​Destructor Called.");
+        }
+        public void display() {
+            Console.WriteLine("Name: " + studentName);
+            Console.WriteLine("Registration Number: " + reg_no);
+        }
+    }
+}
---
+++ Lab 2/test.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace cSharp {
+    class Task {
+        static void Main(string[] args) {
+
+            Console.WriteLine("Student 2");
+            Student std2 = new Student();
+            std2.display();
+        }
+    }
+}
